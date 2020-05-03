de72798811f800d688230d0d8efadbba86edc604
otto puistola <otto.puistola@gmail.com>
2020-05-03 16:46:35 +0300

    temp

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'; //ei tarvitse kirjoittaa myöhemmin react.
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
-import * as moment from 'moment';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Addcustomer from './Addcustomer';
+import Editcustomer from './Editcustomer';
 
 export default function Customerlist(){
 
     const [customers, setCustomers] = useState([]);
     const [trainings, setTrainings] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     var moment = require('moment');
-    moment().format();
 
     useEffect(() => {
         getCustomers();
@@ -28,6 +32,56 @@ export default function Customerlist(){
         .then(response => response.json())
         .then(data => setTrainings(data.content))
         .catch(err => console.error(err))
+    }
+
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')) {
+        fetch(link, {method: 'DELETE'})
+        .then(_ => getCustomers())              
+        .then(_ => {
+            setMsg('Customer deleted');
+            setOpen(true)
+        })
+        .catch(err => console.error(err))
+        }
+    }
+
+    const addCustomer = (customer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers',
+            {
+                method: 'POST', 
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(customer)
+            }
+        )
+        .then(_ => getCustomers())
+        .then(_ => {
+            setMsg('New customer added');
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
+    const updateCustomer = (link, customer) => {
+        fetch(link,{
+            method: 'PUT',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(customer)
+        })
+        .then(_ => getCustomers())
+        .then(_ => {
+            setMsg('Customer updated');
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
+    const handleClose = () => {
+        setOpen(false);
     }
 
 
@@ -59,6 +113,12 @@ export default function Customerlist(){
         {
             Header: 'Phone number',
             accessor: 'phone'
+        },
+        {
+            Cell: row => (<Editcustomer customer={row.original} updateCustomer={updateCustomer} />)
+        },
+        {
+            Cell: row => (<Button onClick={() => deleteCustomer(row.original.links[0].href)} > Delete </Button> )
         }
     ]
 
@@ -84,8 +144,19 @@ export default function Customerlist(){
 
    return(
     <div>
+        <Addcustomer addCustomer={addCustomer}/>
         <ReactTable defaultPageSize={5} filterable={true} data={customers} columns={customercolumns} />
-        <ReactTable defaultPageSize={5} filterable={true} data={trainings} columns={trainingcolumns} /> 
+        <ReactTable defaultPageSize={5} filterable={true} data={trainings} columns={trainingcolumns} />
+        <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            message={msg}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+            }}
+        /> 
     </div>
    );
 }

241a36626dc3c19d760564bf1a725a8c6f49067d
otto puistola <otto.puistola@gmail.com>
2020-11-27 15:38:06 +0200

    prod build

---
+++ build/precache-manifest.efdc3a2a44b68521ea07593a3ac23851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f36796024a11f5e160b2139c92fa889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "880486df989b565bcab3",
+    "url": "/static/css/2.748935a2.chunk.css"
+  },
+  {
+    "revision": "1ba71da9a8304553a9c5",
+    "url": "/static/css/main.e257b699.chunk.css"
+  },
+  {
+    "revision": "880486df989b565bcab3",
+    "url": "/static/js/2.5c8fc6f4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5c8fc6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba71da9a8304553a9c5",
+    "url": "/static/js/main.e675dcea.chunk.js"
+  },
+  {
+    "revision": "0e7bf102b4dc556901f4",
+    "url": "/static/js/runtime-main.f8d9dea5.js"
+  }
+]);
